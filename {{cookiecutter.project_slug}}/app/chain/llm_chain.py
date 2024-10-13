2dbb170619e9b558c744b840556c3261708cce38
janas-adam <a.janas@protonmail.com>
2024-10-13 23:49:15 +0200

    🔨 - applied basic langchain q&a example

--- {{cookiecutter.project_slug}}/app/chain/llm_chain.py
+++ {{cookiecutter.project_slug}}/app/chain/llm_chain.py
@@ -1,1 +1,25 @@
-# define your prompt chains
+from langchain.chains.llm import LLMChain
+from langchain.llms import OpenAI
+from langchain_core.prompts import PromptTemplate
+
+from config import OPENAI_API_KEY
+
+
+def generate_answer(question: str) -> str:
+    llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo-instruct", openai_api_key=OPENAI_API_KEY)
+    prompt = PromptTemplate(
+        input_variables=["question"],
+        template="""
+        You re a helpful and concise assistant.
+        Your task is to answer questions clearly and directly based on the given content.
+        Answer the following question: {question}
+
+        If you feel like you don't have enough information to answer that question, say "I don't know".
+        Your answers should be detailed.
+        """
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+    result = chain.run({"question": question})
+
+    return result.replace("\n", "")
--- {{cookiecutter.project_slug}}/app/main.py
+++ {{cookiecutter.project_slug}}/app/main.py
@@ -1,3 +1,17 @@
+import textwrap
+
 import streamlit as st
+from chain.llm_chain import generate_answer
+
 
 st.title("{{ cookiecutter.project_slug }} 🎉")
+
+with st.sidebar:
+    with st.form("my_form"):
+        question = st.text_area('Ask a question')
+        submit = st.form_submit_button('Submit')
+
+if question and submit:
+    question = question.strip()
+    response = generate_answer(question)
+    st.text(textwrap.fill(response, width=80))
